f154b1301097f7db22619504630748e84cc6dff4
quentin <zhor.osu@gmail.com>
2017-05-31 14:22:51 +0200

    posts

--- wordpress/wp-content/themes/polen/index.php
+++ wordpress/wp-content/themes/polen/index.php
@@ -3,69 +3,77 @@
 <main>
     <img class="topimg" src="<?php echo get_stylesheet_directory_uri( ); ?>/images/lnv.png" alt="logotop">
 
+    <div class="posts">
 
-    <section>
-    <?php
-
-        $args = array('category' => '3');
-        $posts = get_posts($args);
-        $cats = get_category(3);
-        ?>
-        <h4><?php echo $cats->name; ?></h4>
+        <section>
         <?php
-        foreach ($posts as $post) {
-            setup_postdata($post);
+
+        $args = array('category' => '3',
+                        'orderby'  => 'date',
+                        'order'    => 'ASC',);
+            $posts = get_posts($args);
+            $cats = get_category(3);
             ?>
-            <article class='<?php echo $cats->slug; ?>'>
-                <?php the_post_thumbnail( 'full' ) ?>
-                <h5 class="text"><?php the_content() ?></h5>
-            </article>
+            <h4><?php echo $cats->name; ?></h4>
             <?php
-        }
+            foreach ($posts as $post) {
+                setup_postdata($post);
+                ?>
+                <article class='<?php echo $cats->slug; ?>'>
+                    <?php the_post_thumbnail( 'full' ) ?>
+                    <h5 class="text"><?php the_content() ?></h5>
+                </article>
+                <?php
+            }
 
-    ?>
-    </section>
-    <section>
-    <?php
-
-        $args = array('category' => '4');
-        $posts = get_posts($args);
-        $cats = get_category(4);
         ?>
-        <h4><?php echo $cats->name; ?></h4>
+        </section>
+        <section>
         <?php
-        foreach ($posts as $post) {
-            setup_postdata($post);
+
+        $args = array('category' => '4',
+                        'orderby'  => 'date',
+                        'order'    => 'ASC',);
+            $posts = get_posts($args);
+            $cats = get_category(4);
             ?>
-            <article class='<?php echo $cats->slug; ?>'>
-                <?php the_post_thumbnail( 'full' ) ?>
-                <h5 class="text"><?php the_content() ?></h5>
-            </article>
+            <h4><?php echo $cats->name; ?></h4>
             <?php
-        }
+            foreach ($posts as $post) {
+                setup_postdata($post);
+                ?>
+                <article class='<?php echo $cats->slug; ?>'>
+                    <?php the_post_thumbnail( 'full' ) ?>
+                    <h5 class="text"><?php the_content() ?></h5>
+                </article>
+                <?php
+            }
 
-    ?>
-    </section>
-    <section>
-    <?php
-
-        $args = array('category' => '5');
-        $posts = get_posts($args);
-        $cats = get_category(5);
         ?>
-        <h4><?php echo $cats->name; ?></h4>
+        </section>
+        <section>
         <?php
-        foreach ($posts as $post) {
-            setup_postdata($post);
-            ?>
-            <article class='<?php echo $cats->slug; ?>'>
-                <?php the_post_thumbnail( 'full' ) ?>
-                <h5 class="text"><?php the_content() ?></h5>
-            </article>
-            <?php
-        }
 
-    ?>
-    </section>
+            $args = array('category' => '5',
+                            'orderby'  => 'date',
+                            'order'    => 'ASC',);
+            $posts = get_posts($args);
+            $cats = get_category(5);
+            ?>
+            <h4><?php echo $cats->name; ?></h4>
+            <?php
+            foreach ($posts as $post) {
+                setup_postdata($post);
+                ?>
+                <article class='<?php echo $cats->slug; ?>'>
+                    <?php the_post_thumbnail( 'full' ) ?>
+                    <h5 class="text"><?php the_content() ?></h5>
+                </article>
+                <?php
+            }
+
+        ?>
+        </section>
+    </div>
 </main>
 <?php get_footer() ?>
