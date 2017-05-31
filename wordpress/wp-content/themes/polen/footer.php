2cc20ed20b6729cc94e57e08bb103a4f72ddce1b
quentin <zhor.osu@gmail.com>
2017-05-31 11:20:21 +0200

    footer

--- wordpress/wp-content/themes/polen/footer.php
+++ wordpress/wp-content/themes/polen/footer.php
@@ -1,3 +1,32 @@
+<div class="row">
+    <div class="form">
+        <p>Inscription à la Newsletter</p>
+        <form class="" action="#" method="post">
+            <input type="email" name="user_mail" placeholder="" class="champ">
+            <button type="submit" name="button">Envoyer</button>
+        </form>
+    </div>
+    <div class="agenda">
+        <p>Agenda</p>
+        <p>Aucun évènement</p>
+    </div>
+</div>
+<div class="row2">
+    <div class="polenimg">
+        <img src="" alt="">
+        <p>Pôle lozérien d'Economie Numérique</p>
+        <p>Rue du Gévaudan</p>
+        <p>48000 Mende</p>
+        <p>Tel : 04 66 45 33 00 - Fax : 04 66 45 33 00</p>
+        <p>Email : contact@lozere-developpement.com </p>
+    </div>
+    <div class="sponsor">
+        <img src="" alt="">
+    </div>
+</div>
+
+
+
 <?php wp_footer(); ?>
 </body>
 
