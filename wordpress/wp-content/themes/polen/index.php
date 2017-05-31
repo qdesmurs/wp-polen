96af20b64b200d65a3af4bcf7e26518338f903ba
quentin <zhor.osu@gmail.com>
2017-05-31 12:13:02 +0200

    posts

--- wordpress/wp-content/themes/polen/index.php
+++ wordpress/wp-content/themes/polen/index.php
@@ -2,5 +2,32 @@
 <?php get_header() ?>
 <main>
     <img class="topimg" src="<?php echo get_stylesheet_directory_uri( ); ?>/images/lnv.png" alt="logotop">
+
+
+    <section>
+    <?php
+
+
+        $args = array('category' => '3',
+                      'orderby'  => 'date',
+                      'order'    => 'ASC',
+                      'posts_per_page' => 12,);
+        $posts = get_posts($args);
+        $cats = get_the_category();
+        ?>
+        <h4><?php echo $cats[0]->name; ?></h4>
+        <?php
+        foreach ($posts as $post) {
+            setup_postdata($post);
+            ?>
+            <article class='<?php echo $cats[0]->slug; ?>'>
+                <?php the_post_thumbnail( 'full' ) ?>
+                <h5 class="text"><?php the_content() ?></h5>
+            </article>
+            <?php
+        }
+
+    ?>
+    </section>
 </main>
 <?php get_footer() ?>
