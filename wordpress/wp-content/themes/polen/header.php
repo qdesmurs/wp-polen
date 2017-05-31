5507b533f957be6f0ca3a397976072ea6e857b85
quentin <zhor.osu@gmail.com>
2017-05-31 09:50:08 +0200

    plugin

---
+++ wordpress/wp-content/themes/polen/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wp-polen</title>
+    <link rel="stylesheet" href=<?php echo '"'.get_stylesheet_directory_uri( ).'/style.css"';  ?>>
+
+<?php wp_head(); ?>
+</head>
+<body>
+<?php wp_nav_menu( array( 'theme_location' => 'max_mega_menu_1' ) ); ?>
