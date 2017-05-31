79b1873444270a921f88d65f813f507fb70097c4
juandarov <juandarov@gmail.com>
2017-05-31 10:57:13 +0200

    modif sur la navbarre

--- wordpress/wp-content/themes/polen/index.php
+++ wordpress/wp-content/themes/polen/index.php
@@ -1,6 +1,6 @@
 <?php /* Template name: polen */ ?>
 <?php get_header() ?>
 <main>
-
+    <img class="topimg" src="<?php echo get_stylesheet_directory_uri( ); ?>/images/lnv.png" alt="logotop">
 </main>
 <?php get_footer() ?>
